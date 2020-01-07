ddaf76e0a565637d5908ddfb526cc458a01c0925
Gavin Gao <gavin.gao@broadcom.com>
2020-01-07 10:08:30 +0800

    build and extract images

--- extract-artifacts.sh
+++ extract-artifacts.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+
 docker run --rm -i -v ${PWD}:/artifacts \
-attina/xtools-arm-cortex_a8_hf bash << COMMANDS
-cd /home/xtools
-tar zcf x-tools.tar.gz x-tools
-cp x-tools.tar.gz /artifacts
+attina/xtools-sn335x bash << COMMANDS
+make sdk
+cd output
+tar zcf images.tar.gz images
+cp images.tar.gz /artifacts
 COMMANDS
